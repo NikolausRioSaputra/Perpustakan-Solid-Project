a3cd39202b9ea6ca72ff9db3157dbcd473b81139
Nikolaus <nikolausrsaputra@gmail.com>
2024-07-20 16:19:54 +0700

    add list for book on handler, repository, usecase

--- internal/handler/book_handler.go
+++ internal/handler/book_handler.go
@@ -3,12 +3,14 @@ package handler
 import (
 	"Project_Niko/internal/domain"
 	"Project_Niko/internal/usecase"
+	"errors"
 )
 
 type BookHendlerInterface interface {
 	StoreNewBook(book domain.Book) error
 	UpdateBook(book domain.Book) error
 	DeleteBook(book domain.Book) error
+	ListBooks() []domain.Book
 }
 
 type BookHandler struct {
@@ -27,7 +29,7 @@ func (h BookHandler) StoreNewBook(book domain.Book) error {
 		return err
 	}
 
-	return nil
+	return validateBook(book)
 }
 
 func (h BookHandler) UpdateBook(book domain.Book) error {
@@ -36,13 +38,33 @@ func (h BookHandler) UpdateBook(book domain.Book) error {
 		return err
 	}
 
-	return nil
+	return validateBook(book)
 }
 
 func (h BookHandler) DeleteBook(book domain.Book) error {
 	err := h.BookUsecase.DeleteBook(book)
 	if err != nil {
 		return err
+	}
+
+	return validateBook(book)
+}
+
+func (h BookHandler) ListBooks() []domain.Book {
+	err := h.BookUsecase.ListBooks()
+	if err != nil {
+		return err
+	}
+	return h.ListBooks()
+}
+
+func validateBook(book domain.Book) error {
+	if book.ID <= 0 {
+		return errors.New("invalid book ID: must be a positive integer")
+	}
+
+	if book.Title == "" {
+		return errors.New("invalid book title: must be a non-empty string")
 	}
 
 	return nil
